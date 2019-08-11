723ce7510dbd3e53169abc6858ad9b9fbe3fb8b6
Austin Durham <durhamaustin9@gmail.com>
2019-08-10 19:20:41 -0700

    Small Updates

--- javaScript/bird.js
+++ javaScript/bird.js
@@ -4,16 +4,26 @@ $(document).ready(function () {
       let bird = document.createElement('div');
       bird.id = 'birdFrame1';
       bird.classList.add('bird');
+      bird.style.display = 'block';
       document.getElementById('mainBody').appendChild(bird);
 
       let bird2 = document.createElement('div');
       bird2.id = 'birdFrame2';
       bird2.classList.add('bird');
       bird2.style.backgroundPositionX = 'center';
+      bird2.style.display = 'none';
       document.getElementById('mainBody').appendChild(bird2);
 
       let bird3 = document.createElement('div');
+      bird3.id = 'birdFrame3';
+      bird3.classList.add('bird');
+      bird3.style.backgroundPositionX = 'right';
+      bird3.style.display = 'none';
+      document.getElementById('mainBody').appendChild(bird3);
 
+      setInterval(function () {
+
+      }, 1)
     })
   })
 });
